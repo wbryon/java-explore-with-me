8d440dd5a4dd0a8ea4b63a754801e9c9ef2a5697
wbryon <tamirbudaev.habibula@gmail.com>
2023-07-30 21:52:50 +0300

    feat: добавлены контроллеры для сущности Compilation

---
+++ main-service/src/main/java/ru/practicum/ewm/compilation/controller/CompilationPublicController.java
@@ -0,0 +1,37 @@
+package ru.practicum.ewm.compilation.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.ewm.compilation.dto.CompilationDto;
+import ru.practicum.ewm.compilation.service.CompilationService;
+
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/compilations")
+@RequiredArgsConstructor
+public class CompilationPublicController {
+    private final CompilationService compilationService;
+
+    @GetMapping
+    public List<CompilationDto> getCompilations(@RequestParam(defaultValue = "false") Boolean pinned,
+                                                @RequestParam(defaultValue = "0")
+                                                @PositiveOrZero Integer from,
+                                                @RequestParam(defaultValue = "10")
+                                                @Positive Integer size) {
+        return compilationService.getCompilations(pinned, from, size);
+    }
+
+    @GetMapping("/{compId}")
+    public CompilationDto getCompilationById(@PathVariable Long compId) {
+        return compilationService.getCompilationById(compId);
+    }
+}
