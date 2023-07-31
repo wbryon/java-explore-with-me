aca941ebe2a2d82dc78c29624f76b638ec4057ae
wbryon <tamirbudaev.habibula@gmail.com>
2023-07-31 23:53:42 +0300

    feat: Откат к предыдущему коммиту

--- stats/stats-client/src/main/java/ru/practicum/ewm/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/StatsClient.java
@@ -22,8 +22,8 @@ public class StatsClient {
                 .build();
     }
 
-    public void saveHit(EndpointHitDto endpointHitDto) {
-        webClient
+    public EndpointHitDto saveHit(EndpointHitDto endpointHitDto) {
+        return webClient
                 .post()
                 .uri("/hit")
                 .body(Mono.just(endpointHitDto), EndpointHitDto.class)
