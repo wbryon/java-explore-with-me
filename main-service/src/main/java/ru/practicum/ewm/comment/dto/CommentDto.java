85137a9f2de90dc94480d1b3697ff4609d42114b
wbryon <tamirbudaev.habibula@gmail.com>
2023-08-08 22:08:06 +0300

    feat: поле authorId представлено в строковом типе (author)

--- main-service/src/main/java/ru/practicum/ewm/comment/dto/CommentDto.java
+++ main-service/src/main/java/ru/practicum/ewm/comment/dto/CommentDto.java
@@ -2,14 +2,17 @@ package ru.practicum.ewm.comment.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Data
 public class CommentDto {
-    private Long id;
+    @NotBlank
+    @Length(min = 3, max = 512)
     private String text;
-    private Long authorId;
+    private String author;
     private Long eventId;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdOn;
