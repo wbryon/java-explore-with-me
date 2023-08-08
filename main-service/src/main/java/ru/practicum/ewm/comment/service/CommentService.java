73e771dd7f637733c977b2066c982031aebbe041
wbryon <tamirbudaev.habibula@gmail.com>
2023-08-08 22:09:13 +0300

    feat: внесены изменения в метод updateComment

--- main-service/src/main/java/ru/practicum/ewm/comment/service/CommentService.java
+++ main-service/src/main/java/ru/practicum/ewm/comment/service/CommentService.java
@@ -1,14 +1,13 @@
 package ru.practicum.ewm.comment.service;
 
 import ru.practicum.ewm.comment.dto.CommentDto;
-import ru.practicum.ewm.comment.dto.UpdateCommentDto;
 
 import java.util.List;
 
 public interface CommentService {
     CommentDto createComment(CommentDto commentDto, Long userId, Long eventId);
 
-    CommentDto updateComment(UpdateCommentDto commentDto, Long userId, Long commentId);
+    CommentDto updateComment(CommentDto commentDto, Long userId, Long commentId);
 
     CommentDto getCommentByAdmin(Long commentId);
 
